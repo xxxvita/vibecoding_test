cef37b52f63e4806bb53d4dc8df89f9544198a64
XXXVita <whobest.control@gmail.com>
2025-04-01 13:58:10 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import DataGrid from './components/DataGrid';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen p-8">
+      <h1 className="text-3xl font-bold mb-8 text-center">Advanced Data Grid</h1>
+      <DataGrid />
+    </div>
+  );
+}
